819983635d83b5887e69377b34dea50e80730db9
HyeongSeoku <gudtjr3437@naver.com>
2022-02-18 10:58:34 +0900

    feat : Route 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import LoginModal from "./components/LoginModal";
+import Home from "./pages/Home";
 
-function App() {
-  return <div></div>;
+function App(): JSX.Element {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
