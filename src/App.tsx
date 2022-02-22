2978a352f18b0f41f950a9ab3a10ffbf7ea1b84f
HyeongSeoku <gudtjr3437@naver.com>
2022-02-23 00:07:58 +0900

    feat : 로그인 한 유저 페이지 경로 설정 (임시)

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginModal from "./components/LoginModal";
 import Home from "./pages/Home";
+import LoggedInHome from "./pages/LoggedInHome";
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/loggedInHome" element={<LoggedInHome />} />
     </Routes>
   );
 }
