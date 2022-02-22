ba66573a6e96a695beb3c797348cc22ced642d2c
HyeongSeoku <gudtjr3437@naver.com>
2022-02-23 00:12:43 +0900

    feat : 검색 바 컴포넌트

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -5,7 +5,7 @@ const SearchBar = () => {
   return (
     <div className="w-1/3 h-16  rounded-2xl flex box-border p-2 items-baseline  bg-slate-300 focus:bg-slate-400 ">
       <FontAwesomeIcon icon={faSearch} className="flex-grow-0 text-lg ml-2" />
-      <input className="grow h-full rounded-2xl box-border p-3 text-lg ml-2 bg-inherit focus:outline-none" />
+      <input className="grow w-full h-full rounded-2xl box-border p-3 text-lg ml-2 bg-inherit focus:outline-none" />
     </div>
   );
 };
