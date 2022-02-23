8e2fb168ce8644d31fa9b0666a1580f46a3f9840
HyeongSeoku <gudtjr3437@naver.com>
2022-02-23 16:19:14 +0900

    feat : SearchBar 크기 반응형 적용

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -3,7 +3,7 @@ import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const SearchBar = () => {
   return (
-    <div className="w-1/3 h-16  rounded-2xl flex box-border p-2 items-baseline  bg-slate-300 focus:bg-slate-400 ">
+    <div className="sm:w-100 lg:w-1/3 h-16  rounded-2xl flex box-border p-2 items-baseline  bg-slate-300 focus:bg-slate-400 ">
       <FontAwesomeIcon icon={faSearch} className="flex-grow-0 text-lg ml-2" />
       <input className="grow w-full h-full rounded-2xl box-border p-3 text-lg ml-2 bg-inherit focus:outline-none" />
     </div>
