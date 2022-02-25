2604994bea96a3ac87a384444901ec4925d16615
HyeongSeoku <gudtjr3437@naver.com>
2022-02-26 00:12:30 +0900

    feat : 비디오 분석 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,14 @@ import "./App.css";
 import LoginModal from "./components/LoginModal";
 import Home from "./pages/Home";
 import MyPage from "./pages/MyPage";
+import VideoAnalysis from "./pages/VideoAnalysis";
 
 function App(): JSX.Element {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/mypage" element={<MyPage />} />
+      <Route path="analysis" element={<VideoAnalysis />} />
     </Routes>
   );
 }
