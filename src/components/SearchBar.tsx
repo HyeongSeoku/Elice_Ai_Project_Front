5fc9b077f4865a2faac3c3b91a2155e013bd1f06
HyeongSeoku <gudtjr3437@naver.com>
2022-02-26 11:54:25 +0900

    feat : 로딩 모달 추가 및 제어

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,27 +1,40 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { SearchTypes } from "SearchModule";
 
-const SearchBar = () => {
+const SearchBar = ({
+  isLoading,
+  onChangeLoadingState,
+}: SearchTypes.searchProps) => {
   const [searchWord, setSearchWord] = useState<string>("");
 
   const onSearchWordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
-    setSearchWord(value);
+    //[임시] 로딩 중일때 이벤트 막아놓음
+    if (!isLoading) {
+      setSearchWord(value);
+    }
   };
 
   const onSubmitSearch = (e: React.SyntheticEvent) => {
     e.preventDefault();
-    //데이터 분석 요청 메소드
-    //로딩중으로 상태 변경
-    //요청 잘 올 경우 분석 페이지로 이동
-    setSearchWord("");
+    //데이터 분석 요청 메소드 (비동기 처리)
+    if (!isLoading) {
+      onChangeLoadingState();
+      //TODO : 요청한 데이터 잘 올떄까지 기다린후 로딩 false로 변환
+      setTimeout(() => {
+        onChangeLoadingState();
+      }, 4000);
+      //요청 잘 올 경우 분석 페이지로 이동
+      setSearchWord("");
+    }
   };
 
   return (
     <div className="sm:w-100 lg:w-1/3 h-16  rounded-2xl flex box-border p-2 items-baseline  bg-slate-300 focus:bg-slate-400 ">
       <FontAwesomeIcon icon={faSearch} className="flex-grow-0 text-lg ml-2" />
-      <form onSubmit={onSubmitSearch}>
+      <form className="w-full" onSubmit={onSubmitSearch}>
         <input
           className="grow w-full h-full rounded-2xl box-border p-3 text-lg ml-2 bg-inherit focus:outline-none"
           value={searchWord}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { LoadingModal } from "../components";
 import HeaderMenu from "../components/HeaderMenu";
 import LoginModal from "../components/LoginModal";
 import Logo from "../components/Logo";
@@ -7,23 +8,32 @@ import SearchBar from "../components/SearchBar";
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
   const onModalBtnClick = () => {
     setIsModalOpen((current) => !current);
   };
 
+  const onChangeLoadingState = () => {
+    setIsLoading((current) => !current);
+  };
+
   return (
     <div className="container">
       <HeaderMenu isModalOpen={isModalOpen} onModalBtnClick={onModalBtnClick} />
       <Logo />
-      <SearchBar />
+      <SearchBar
+        isLoading={isLoading}
+        onChangeLoadingState={onChangeLoadingState}
+      />
       {isModalOpen && (
         <LoginModal
           isModalOpen={isModalOpen}
           onModalBtnClick={onModalBtnClick}
         />
       )}
+      {isLoading && <LoadingModal />}
     </div>
   );
 };
