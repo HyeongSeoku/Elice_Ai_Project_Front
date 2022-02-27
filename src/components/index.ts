90100874784d202478da7cb4a97952d8552718ab
HyeongSeoku <gudtjr3437@naver.com>
2022-02-27 17:07:16 +0900

    chore : components index 파일

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as HeaderMenu } from "./HeaderMenu";
 export { default as LoginModal } from "./LoginModal";
 export { default as Logo } from "./Logo";
 export { default as VideoCard } from "./VideoCard";
+export { default as LoadingModal } from "./LoadingModal";
