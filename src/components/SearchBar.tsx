d226355c92db383af988fc4cada47970f35841e7
HyeongSeoku <gudtjr3437@naver.com>
2022-02-28 03:03:02 +0900

    feat : 비디오 검색 후 결과 페이지로 이동

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -2,12 +2,95 @@ import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { SearchTypes } from "SearchModule";
+import { useNavigate } from "react-router-dom";
+import analysisApi from "../apis/analysisApi";
 
 const SearchBar = ({
   isLoading,
   onChangeLoadingState,
 }: SearchTypes.searchProps) => {
   const [searchWord, setSearchWord] = useState<string>("");
+  const navigate = useNavigate();
+
+  const testData = [
+    {
+      id: "중요도",
+      data: [
+        {
+          x: "00:00:00",
+          y: 0.0,
+        },
+        {
+          x: "00:00:10",
+          y: 0.4,
+        },
+        {
+          x: "00:00:20",
+
+          y: 1.0,
+        },
+        {
+          x: "00:00:30",
+          y: 1.4,
+        },
+        {
+          x: "00:00:40",
+          y: 2.5,
+        },
+        {
+          x: "00:00:50",
+
+          y: 3.5,
+        },
+        {
+          x: "00:01:50",
+
+          y: 6.5,
+        },
+        {
+          x: "00:02:50",
+
+          y: 1.5,
+        },
+        {
+          x: "00:03:50",
+
+          y: 0.5,
+        },
+        {
+          x: "00:04:50",
+
+          y: 30.5,
+        },
+        {
+          x: "00:05:50",
+
+          y: 1.5,
+        },
+        {
+          x: "00:06:50",
+
+          y: 10.5,
+        },
+        {
+          x: "00:07:50",
+
+          y: 2.5,
+        },
+      ],
+    },
+  ];
+
+  const testAnalyzedData = {
+    videoId: 1,
+    videoUrl: "https://www.youtube.com/embed/3AyMjyHu1bA",
+    thumbnail: "Asdv",
+    owner: "testOwner",
+    title: "testTitle",
+    keywordCnt: [{ keyword: "sdvsdv", count: 10 }],
+    timeStamp: testData,
+    timStampContents: [{ time: "00:00:01", contents: "avasd" }],
+  };
 
   const onSearchWordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target;
@@ -20,14 +103,34 @@ const SearchBar = ({
   const onSubmitSearch = (e: React.SyntheticEvent) => {
     e.preventDefault();
     //데이터 분석 요청 메소드 (비동기 처리)
+
     if (!isLoading) {
       onChangeLoadingState();
       //TODO : 요청한 데이터 잘 올떄까지 기다린후 로딩 false로 변환
-      setTimeout(() => {
-        onChangeLoadingState();
-      }, 4000);
-      //요청 잘 올 경우 분석 페이지로 이동
-      setSearchWord("");
+    }
+    setTimeout(() => {
+      // sendTimeStampAnalysis();
+      onChangeLoadingState();
+      navigate("/analysis", {
+        state: { analyzedVideo: testAnalyzedData, timeStamp: testData },
+      }); //요청 잘 올 경우 분석 페이지로 이동
+    }, 4000);
+    setSearchWord("");
+  };
+
+  const sendTimeStampAnalysis = async () => {
+    try {
+      await analysisApi.getTimeStamp(searchWord).then((response: any) => {
+        if (response.status === 200) {
+          const analyzedData = response.data;
+          onChangeLoadingState();
+          navigate("/analysis", {
+            state: { analyzedVideo: analyzedData, timeStamp: testData },
+          }); //요청 잘 올 경우 분석 페이지로 이동
+        }
+      });
+    } catch (e) {
+      console.log(e);
     }
   };
 
