95753721d357642366c539cdf85ecf9f68b37ec2
HyeongSeoku <gudtjr3437@naver.com>
2022-02-28 03:02:28 +0900

    feat : 비디오 분석 결과 페이지 작성(더미 데이터)

--- src/pages/VideoAnalysis.tsx
+++ src/pages/VideoAnalysis.tsx
@@ -1,12 +1,39 @@
+import { useLocation } from "react-router-dom";
 import { VideoAnalysisTypes } from "VideoAnalysisModule";
+import { ResponsiveLine } from "@nivo/line";
 
-const VideoAnalysis = ({
-  analyzedVideo,
-}: VideoAnalysisTypes.videoAnalysisProps) => {
+const VideoAnalysis = () => {
+  const location = useLocation();
+  const state = location.state as VideoAnalysisTypes.videoAnalysisProps;
+  console.log(state);
+  const analysisData = state.analyzedVideo;
+  const timeStampData = state.timeStamp;
+
   return (
     <div className="container">
-      <section>
-        <iframe></iframe>
+      <h1>분석</h1>
+      <section className="w-full h-full flex flex-col justify-center items-center">
+        <iframe src={analysisData.videoUrl}></iframe>
+        <div>{analysisData.title}</div>
+        <div>{analysisData.owner}</div>
+        <div className="w-full h-52">
+          <ResponsiveLine
+            curve={"monotoneX"}
+            data={timeStampData}
+            xScale={{ type: "point" }}
+            yScale={{
+              type: "linear",
+              min: "auto",
+              max: "auto",
+              stacked: true,
+              reverse: false,
+            }}
+            lineWidth={1}
+            pointSize={0}
+            useMesh={true}
+            crosshairType="cross"
+          />
+        </div>
       </section>
     </div>
   );
