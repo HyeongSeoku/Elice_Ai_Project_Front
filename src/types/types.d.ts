843c0a63fad70dfad6494ac4985ee1dd146f0ef3
HyeongSeoku <gudtjr3437@naver.com>
2022-02-27 17:06:26 +0900

    type 추가

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -16,13 +16,32 @@ declare module "VideoAnalysisModule" {
   export namespace VideoAnalysisTypes {
     interface videoAnalysisProps {
       analyzedVideo: {
+        videoId: number;
         videoUrl: string;
         thumbnail: string;
         owner: string;
         title: string;
-        hashtag: string[];
-        mainSection: string[];
+        keywordCnt: { keyword: string; count: number }[];
+        timeStamp: {
+          id: string;
+          color: string;
+          data: { time: number; score: number }[];
+        }[];
+        timStampContents: { time: number; contents: string }[];
       };
+      timeStamp: {
+        id: string;
+        data: { x: number; y: number }[];
+      }[];
+    }
+  }
+}
+
+declare module "SearchModule" {
+  export namespace SearchTypes {
+    interface searchProps {
+      isLoading: boolean;
+      onChangeLoadingState: () => void;
     }
   }
 }
