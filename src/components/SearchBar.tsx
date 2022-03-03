99072499a1bb6e9e0c9324178273bd9b36034fae
HyeongSeoku <gudtjr3437@naver.com>
2022-03-04 01:13:07 +0900

    feat : 비디오 분석 api 연결

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { SearchTypes } from "SearchModule";
@@ -10,7 +10,15 @@ const SearchBar = ({
   onChangeLoadingState,
 }: SearchTypes.searchProps) => {
   const [searchWord, setSearchWord] = useState<string>("");
+  const [videoId, setVideoId] = useState<number>(0);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (videoId !== 0) {
+      //api 실행
+      getDetailInfoVideo(videoId);
+    }
+  }, [videoId]);
 
   const testData = [
     {
@@ -97,36 +105,71 @@ const SearchBar = ({
     //[임시] 로딩 중일때 이벤트 막아놓음
     if (!isLoading) {
       setSearchWord(value);
+      return;
     }
   };
 
+  //TODO:예외 처리
+  //1.영상의 아이디가 특정되지 않았을 경우
+  //2.URL이 영상이 아닐 경우
+  //3.분석이 불가능할 경우
   const onSubmitSearch = (e: React.SyntheticEvent) => {
     e.preventDefault();
+    //문자열에서 유튜브 영상의 아이디값 저장
+    const urlStr: string = getUrlId(searchWord);
     //데이터 분석 요청 메소드 (비동기 처리)
-
     if (!isLoading) {
       onChangeLoadingState();
+      getVideoId(urlStr);
       //TODO : 요청한 데이터 잘 올떄까지 기다린후 로딩 false로 변환
+      return;
     }
-    setTimeout(() => {
-      // sendTimeStampAnalysis();
-      onChangeLoadingState();
-      navigate("/analysis", {
-        state: { analyzedVideo: testAnalyzedData, timeStamp: testData },
-      }); //요청 잘 올 경우 분석 페이지로 이동
-    }, 4000);
-    setSearchWord("");
   };
 
-  const sendTimeStampAnalysis = async () => {
+  //검색어에서 유튜브 영상의 아이디만 추출
+  const getUrlId = (text: string) => {
     try {
-      await analysisApi.getTimeStamp(searchWord).then((response: any) => {
+      if (text.includes("watch")) {
+        const result = text.split("v=");
+        return result[result.length - 1];
+      } else {
+        const result = text.split("/");
+        return result[result.length - 1];
+      }
+      //catch문으로 갈수 있는 경우 입력 필요할듯
+    } catch (e) {
+      alert("URL 형식 에러!");
+      //string반환 때문에 작성
+      return "error";
+    }
+  };
+
+  //분석 요청시 가장 처음 실행되는 메소드 1번 함수
+  const getVideoId = async (text: string) => {
+    try {
+      await analysisApi.getVideoId({ source: text }).then((response: any) => {
+        if (response.status === 201) {
+          console.log("api 요청 성공");
+          setVideoId(response.data.video_id);
+        }
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  //리턴 받은 비디오 아이디를 통해 비디오 세부 디테일을 받아오는 메소드 2번함수
+  const getDetailInfoVideo = async (videoId: number) => {
+    try {
+      await analysisApi.getVideoDetail(videoId).then((response: any) => {
         if (response.status === 200) {
-          const analyzedData = response.data;
-          onChangeLoadingState();
-          navigate("/analysis", {
-            state: { analyzedVideo: analyzedData, timeStamp: testData },
-          }); //요청 잘 올 경우 분석 페이지로 이동
+          const tmpData = response.data;
+          console.log(response.data);
+          setTimeout(() => {
+            onChangeLoadingState();
+            setSearchWord("");
+            navigate("/analysis", { state: { analyzedVideo: tmpData } });
+          }, 4000);
         }
       });
     } catch (e) {
