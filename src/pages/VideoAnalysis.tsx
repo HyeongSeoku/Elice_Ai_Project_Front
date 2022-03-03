df6a63b16ee097a2d94a8e66d7759e986cb4957f
HyeongSeoku <gudtjr3437@naver.com>
2022-03-04 01:13:28 +0900

    feat : 비디오 분석 화면 수정 [임시]

--- src/pages/VideoAnalysis.tsx
+++ src/pages/VideoAnalysis.tsx
@@ -5,21 +5,32 @@ import { ResponsiveLine } from "@nivo/line";
 const VideoAnalysis = () => {
   const location = useLocation();
   const state = location.state as VideoAnalysisTypes.videoAnalysisProps;
-  console.log(state);
   const analysisData = state.analyzedVideo;
-  const timeStampData = state.timeStamp;
+  const tmpId = analysisData.source.split("v=");
+
+  const videoId = tmpId[tmpId.length - 1];
+  console.log(videoId);
+  //데이터 처리
+  const timeStampData: any[] = [];
+
+  analysisData.time_keywords.forEach((item) => {
+    timeStampData.push({ x: item.timestamp, y: item.score });
+  });
+  const graphData: any[] = [{ id: "분석 데이터", data: timeStampData }];
+  console.log(timeStampData);
+  console.log(state);
 
   return (
     <div className="container">
       <h1>분석</h1>
       <section className="w-full h-full flex flex-col justify-center items-center">
-        <iframe src={analysisData.videoUrl}></iframe>
+        <iframe src={`https://www.youtube.com/embed/${videoId}`}></iframe>
         <div>{analysisData.title}</div>
-        <div>{analysisData.owner}</div>
+        <div>{analysisData.author}</div>
         <div className="w-full h-52">
           <ResponsiveLine
             curve={"monotoneX"}
-            data={timeStampData}
+            data={graphData}
             xScale={{ type: "point" }}
             yScale={{
               type: "linear",
