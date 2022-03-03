b4e992820164647c80b9c0955b55e052bdfe9a5e
HyeongSeoku <gudtjr3437@naver.com>
2022-03-04 01:14:00 +0900

    chore : API 엔드포인트 상수화

--- src/constants/standard.ts
+++ src/constants/standard.ts
@@ -2,3 +2,6 @@ export const ID_MIN_LENGTH = 6;
 export const ID_MAX_LENGTH = 20;
 export const PWD_MIN_LENGTH = 8;
 export const PWD_MAX_LENGTH = 16;
+
+export const API_END_POINT =
+  "http://elice-kdt-ai-3rd-team05.koreacentral.cloudapp.azure.com:5000";
