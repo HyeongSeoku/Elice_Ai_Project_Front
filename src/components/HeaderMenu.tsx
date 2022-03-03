7b3f482279be0de36bb14d5092f03ed51f3b6e84
HyeongSeoku <gudtjr3437@naver.com>
2022-03-04 01:11:21 +0900

    feat : 로그아웃 기능 추가

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useRecoilValue } from "recoil";
+import { useRecoilState } from "recoil";
 import { RecoilProps } from "RecoilModule";
 import { loginState } from "../atom";
 
@@ -10,20 +10,39 @@ interface Props {
 }
 
 const HeaderMenu = ({ isModalOpen, onModalBtnClick }: Props) => {
-  const isLoggedIn = useRecoilValue<boolean>(loginState);
+  const [isLoggedIn, setIsLoggedIn] = useRecoilState<boolean>(loginState);
 
   useEffect(() => {
     console.log(isLoggedIn);
   }, [isLoggedIn]);
 
+  const onChangeLoginState = () => {
+    if (isLoggedIn) {
+      const logMsg = window.confirm("로그아웃 하시겠습니까?");
+      if (logMsg) {
+        setIsLoggedIn(false);
+      } else {
+        return;
+      }
+    }
+  };
+
   return (
-    <header className="w-full flex flex-row justify-end fixed top-0 right-0 left-0 space-x-2 box-border p-3">
+    <div className="w-full flex flex-row justify-end fixed top-0 right-0 left-0 space-x-2 box-border p-3">
       {isLoggedIn && (
-        <Link to="/mypage">
-          <button className="bg-slate-200 py-2 px-5 rounded-xl hover:bg-slate-400">
-            마이 페이지 임시 버튼
+        <div className="flex space-x-2 ">
+          <Link to="/mypage">
+            <button className="bg-slate-200 py-2 px-5 rounded-xl hover:bg-slate-400">
+              마이 페이지 임시 버튼
+            </button>
+          </Link>
+          <button
+            className="bg-red-200 py-2 px-5 rounded-xl hover:bg-red-400"
+            onClick={onChangeLoginState}
+          >
+            로그아웃
           </button>
-        </Link>
+        </div>
       )}
       {!isLoggedIn && (
         <div className="flex space-x-2 ">
@@ -40,7 +59,7 @@ const HeaderMenu = ({ isModalOpen, onModalBtnClick }: Props) => {
           </Link>
         </div>
       )}
-    </header>
+    </div>
   );
 };
 
