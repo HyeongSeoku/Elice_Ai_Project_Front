ff00e5d76d05691d8e81388fdfd0447093cf9a1f
HyeongSeoku <gudtjr3437@naver.com>
2022-03-04 21:25:36 +0900

    feat : 스크립트 아이템 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { default as LoginModal } from "./LoginModal";
 export { default as Logo } from "./Logo";
 export { default as VideoCard } from "./VideoCard";
 export { default as LoadingModal } from "./LoadingModal";
+export { default as ScriptItem } from "./AnalysisScriptItem";
