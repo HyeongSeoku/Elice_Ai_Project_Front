68b8de4c609c4426f6d4b5997e123c5aa1b82f6b
HyeongSeoku <gudtjr3437@naver.com>
2022-03-13 04:28:32 +0900

    feat : not found page 추가

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as Home } from "./Home";
 export { default as MyPage } from "./MyPage";
 export { default as VideoAnalysis } from "./VideoAnalysis";
 export { default as Regist } from "./Regist";
+export { default as NotFounErrorPage } from "./NotFoundErrorPage";
