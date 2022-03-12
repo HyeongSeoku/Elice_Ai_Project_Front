a31cacb39fd14e91996c9c78274b4afe8c617af2
HyeongSeoku <gudtjr3437@naver.com>
2022-03-12 19:53:58 +0900

    stlye : 홈 ui 변경

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useRecoilState, useRecoilValue } from "recoil";
-import { loginState, modalState } from "../atom";
+import { loginState, loginUser, modalState } from "../atom";
 import { LoadingModal } from "../components";
 import HeaderMenu from "../components/HeaderMenu";
 import LoginModal from "../components/LoginModal";
 import Logo from "../components/Logo";
 import SearchBar from "../components/SearchBar";
 import MainLayout from "../components/layout/MainLayout";
+
+import searchMainSrc from "../img/hash_main/titleColor2.png";
+import youtubeMainSrc from "../img/hash_main/youtubeBlack.png";
+import getSearchMainSrc from "../img/hash_main/getSearch_color.png";
+
 import intro1_1 from "../img/hash/page1-1.png";
 import intro1_2 from "../img/hash/page1-2.png";
 import intro1_3 from "../img/hash/page1-3.png";
@@ -27,6 +32,7 @@ import intro7_1 from "../img/hash/page7-1.png";
 import intro7_2 from "../img/hash/page7-2.png";
 import intro7_3 from "../img/hash/page7-3.png";
 import intro7_4 from "../img/hash/page7-4.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Home = () => {
   const isModalOpen = useRecoilValue(modalState);
@@ -53,9 +59,39 @@ const Home = () => {
 
   return (
     <div className="  mx-auto p-5  overflow-scroll">
-      <HeaderMenu scroll={scrollPosition} />
+      <HeaderMenu />
       <section
-        className="flex flex-col justify-center items-center mx-auto p-5  overflow-hidden relative"
+        className="flex flex-col justify-center items-center mx-auto p-5  overflow-hidden"
+        style={{ width: "100%", height: "100vh" }}
+      >
+        <div className="flex justify-center items-center">
+          <img src={youtubeMainSrc} className="w-96" />
+        </div>
+        <div className="flex justify-center items-center">
+          <img src={getSearchMainSrc} className="w-96" />
+        </div>
+        <div className="mt-auto">
+          <FontAwesomeIcon icon="fa-solid fa-angle-down" />
+        </div>
+        <div
+          className="flex justify-center items-center"
+          style={{
+            opacity: (scrollPosition - 120.5) / 20,
+          }}
+        >
+          <img src={intro1_2} className="w-80" />
+        </div>
+        <div
+          className="flex justify-center items-center"
+          style={{
+            opacity: (scrollPosition - 122.5) / 20,
+          }}
+        >
+          <img src={intro1_3} className="w-80" />
+        </div>
+      </section>
+      <section
+        className="flex flex-col justify-center items-center mx-auto p-5  overflow-hidden"
         style={{ width: "100%", height: "100vh" }}
       >
         <div className="font-bold text-4xl mb-5">
@@ -65,34 +101,15 @@ const Home = () => {
           isLoading={isLoading}
           onChangeLoadingState={onChangeLoadingState}
         />
+
+        <div className="text-slate-500 mt-3">
+          1시간 이내 유튜브 URL만 입력해주세요.
+        </div>
+        <div className="text-slate-500 text-sm">
+          실시간 방송과 한국어 외 다른 언어의 영상은 지원하지 않습니다
+        </div>
         {isModalOpen && <LoginModal />}
         {isLoading && <LoadingModal />}
-      </section>
-      <section>
-        <div
-          className="flex justify-center items-center"
-          style={{
-            opacity: (scrollPosition - 100.5) / 10,
-          }}
-        >
-          <img src={intro1_1} className="w-64" />
-        </div>
-        <div
-          className="flex justify-center items-center"
-          style={{
-            opacity: (scrollPosition - 120.5) / 20,
-          }}
-        >
-          <img src={intro1_2} className="w-72" />
-        </div>
-        <div
-          className="flex justify-center items-center"
-          style={{
-            opacity: (scrollPosition - 122.5) / 20,
-          }}
-        >
-          <img src={intro1_3} className="w-64" />
-        </div>
       </section>
     </div>
   );
