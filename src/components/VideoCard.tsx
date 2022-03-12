a732fe5eac77addbb57625b410dcfd20d1e90d47
HyeongSeoku <gudtjr3437@naver.com>
2022-03-13 04:32:00 +0900

    feat : 비디오 카드 클릭시 분석화면으로 이동 , 세부 스타일 조정

--- src/components/VideoCard.tsx
+++ src/components/VideoCard.tsx
@@ -1,24 +1,46 @@
+import { useNavigate } from "react-router-dom";
 import { VideoCardTypes } from "VideoCardModule";
 
-const VideoCard = ({ videoObj }: VideoCardTypes.videoCardProps) => {
+const VideoCard = ({
+  videoObj,
+  getVideoDetail,
+}: VideoCardTypes.videoCardProps) => {
+  const navigate = useNavigate();
+
+  const detailApiRequest = async () => {
+    const videoDetail = await getVideoDetail(videoObj.id);
+
+    navigate("/analysis", { state: { analyzedVideo: videoDetail } });
+  };
+
   return (
-    <div className="videoCardContainer hover:scale-105 hover:shadow-2xl ">
-      <div className="w-full h-auto flex flex-col" style={{ flexGrow: 1 }}>
+    <div
+      className="videoCardContainer hover:scale-105 hover:shadow-2xl box-border p-5 mt-10 mb-10"
+      onClick={detailApiRequest}
+    >
+      <div
+        className="w-full h-auto flex flex-col bg-slate-200 box-border"
+        style={{ flexGrow: 1 }}
+      >
         <div className="videoContentsRowContainer">
           <div className="bg-black rounded-full w-full h-auto">
-            <iframe
-              title={videoObj.title}
+            <img
+              alt={videoObj.title}
               className="w-full h-full"
-              src={`https://www.youtube.com/embed/${videoObj.youtube_slug}`}
+              src={videoObj.thumbnail}
             />
           </div>
-          <div className="text-lg font-bold">영상 제목</div>
+          <div className="text-md font-bold">{videoObj.title}</div>
         </div>
-        <div className="font-light ml-7 flex-grow">만든 사람</div>
+        <div className="font-light ml-3 flex-grow">{videoObj.author}</div>
         <div className="videoContentsRowContainer">
-          {/* {videoObj.hashTag.map((item) => (
-            <div className="text-lg font-light">#{item}</div>
-          ))} */}
+          {videoObj.keywords_frequency.map((item, idx) =>
+            idx < 3 ? (
+              <div className="text-lg font-light">#{item.keyword}</div>
+            ) : (
+              ""
+            )
+          )}
         </div>
       </div>
     </div>
